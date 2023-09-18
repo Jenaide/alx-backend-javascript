f80392d073d6d21b0cf7559772fa31cf5329f6ab
Jenaide <jenaidesibolie@gmail.com>
2023-09-18 18:15:49 +0000

    test

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,10 @@
 /**
- * a function to display a message on STDOUT
- **/
-const displayMessage = (message) => {
-  console.log(message);
-}
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
