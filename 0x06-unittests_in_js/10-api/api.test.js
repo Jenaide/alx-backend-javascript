5a2f653fba07f5e9d29ef28e8a1aaa460e4c4958
Jenaide <jenaidesibolie@gmail.com>
2023-09-22 19:38:08 +0000

    bug fix

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
 
-  it('GET / returns correct response', (done) => {
+  it('should returns the correct response', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
@@ -12,7 +12,7 @@ describe('API integration test', () => {
     });
   });
 
-  it('GET /cart/:id returns correct response for valid :id', (done) => {
+  it('Should returns correct response for valid :id', (done) => {
     request.get(`${API_URL}/cart/47`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 47');
@@ -20,21 +20,21 @@ describe('API integration test', () => {
     });
   });
 
-  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
+  it('should returns 404 response for negative number values in :id', (done) => {
     request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
 
-  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+  it('should returns 404 response for non-numeric values in :id', (done) => {
     request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
 
-  it('POST /login returns valid response', (done) => {
+  it('should login and return a valid response', (done) => {
     request.post(`${API_URL}/login`, {json: {userName: 'Pinkbrook'}}, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome Pinkbrook');
@@ -42,7 +42,7 @@ describe('API integration test', () => {
     });
   });
 
-  it('GET /available_payments returns valid response', (done) => {
+  it('should return available_payments with a valid response', (done) => {
     request.get(`${API_URL}/available_payments`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(JSON.parse(body))
